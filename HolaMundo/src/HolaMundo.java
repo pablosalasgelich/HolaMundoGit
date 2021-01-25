e6c6f86e3e5ad070c65ea8b4c5a6acc403c1793d
pablo3389 <pablo_3389@hotmail.com>
2021-01-25 08:33:47 +0100

    1er cambio - Eclipse

--- HolaMundo/src/HolaMundo.java
+++ HolaMundo/src/HolaMundo.java
@@ -1,8 +1,8 @@
 public class HolaMundo {
 
 	public static void main(String[] args) {
-		System.out.println("Hola mundo!");
-
+		System.out.println("1er cambio - Consola");
+		System.out.println("1er cambio - Eclipse");
 	}
 
 }
